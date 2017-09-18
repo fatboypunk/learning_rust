99bacb5e6b501ca786ff5ed3355d2f20660615b4
Marcel Horlings <marcel.horlings@gmail.com>
2017-09-19 00:14:34 +0200

    add user input and rand for random numbers

---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/Users/marcel/code/rust_projects/guessing_game/target/debug/guessing_game: /Users/marcel/code/rust_projects/guessing_game/src/main.rs
---
+++ guessing_game/target/debug/libguessing_game.d
@@ -0,0 +1,1 @@
+/Users/marcel/code/rust_projects/guessing_game/target/debug/libguessing_game.rmeta: /Users/marcel/code/rust_projects/guessing_game/src/main.rs
