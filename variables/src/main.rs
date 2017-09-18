8e404e15d8deebca7cbcf6f97133aaa7ac6572d6
Marcel Horlings <marcel.horlings@gmail.com>
2017-09-19 01:19:17 +0200

    redefine y with shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,17 @@
 fn main() {
     let mut x = 5;
     println!("The value of x is: {}", x);
+    // The value of x is: 5
     x = 6;
     println!("The value of x is: {}", x);
+    // The value of x is: 6
+
+    let y = 5;
+
+    let y = y + 1;
+
+    let y = y * 2;
+
+    println!("The value of y is: {}", y);
+    // The value of x is: 12
 }
