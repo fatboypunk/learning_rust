41f65e13b9140dcfb9dc7655352fa1791dc046de
Marcel Horlings <marcel.horlings@gmail.com>
2018-08-25 16:14:43 +0200

    add rectangles project for struct examples

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     println!("hello world!");
 
-    another_function();
+    another_function(5, 6);
 }
 
-fn another_function() {
-    println!("another function!");
+fn another_function(x: i32, y: i32) {
+    println!("the value of x is {}", x);
+    println!("the value of y is {}", y);
 }
